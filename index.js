2ce03c0b0bca14b5eb39606e3915ba6bb2cb86c6
Alen Kelemen <alen@edc.hr>
2021-03-29 15:00:33 +0200

    ...

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 import { elt } from "./util";
+import moment from "moment";
 
 const header = elt("h1", {}, "Mjerenja na vodoopskrbnoj mreži");
 document.body.appendChild(header);
-
+const startDate = elt("input", { type: "datetime-local" });
 const deviceSelector = elt("select", {});
+startDate.value = moment().subtract(1, "days").format("YYYY-MM-DDTHH:mm");
+const endDate = elt("input", { type: "datetime-local" });
+endDate.value = moment().format("YYYY-MM-DDTHH:mm");
 const valuesSelector = elt("select", {});
-
+console.log(endDate.value);
 const fielset = elt(
   "fieldset",
   {},
   elt("label", {}, "Odaberi uređaj"),
   deviceSelector,
+  elt("label", {}, "Početni datum"),
+  startDate,
+  elt("label", {}, "Završni datum"),
+  endDate,
   elt("label", {}, "Izmjereno"),
   valuesSelector
 );
@@ -21,10 +29,12 @@ fetch("https://gis.edc.hr/imagisth/threport/device?info_id=eq.2")
   .then((response) => response.json())
   .then((data) => {
     for (const i of data) {
-      deviceSelector.options.add(new Option(i.device_name, i.device_id));
+      const o = new Option(i.device_name, i.device_id);
+      deviceSelector.options.add(o);
+      if (i.device_id === 177) o.selected = true;
     }
   });
-deviceSelector.addEventListener("change", (evt) => {
+const deviceSelectorChanged = (evt) => {
   const deviceId = evt.target.value;
   valuesSelector.length = 0;
   fetch(
@@ -32,20 +42,12 @@ deviceSelector.addEventListener("change", (evt) => {
   )
     .then((response) => response.json())
     .then((data) => {
-        
+      const startUnixDate = moment(startDate.value).unix();
+      const endUnixDate = moment(endDate.value).unix();
+      console.log(startUnixDate);
       for (const i of data) {
-        console.log(i);
-        
-        valuesSelector.options.add(
-          new Option(i.date_part + ":" + i.pressure)
-        );
+        console.log(i.date_part);
       }
     });
-  /*  fetch("https://gis.edc.hr/imagisth/threport/flow_th?device_id=eq." + deviceId)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      //valuesSelector.length=0;
-      //valuesSelector.options.add(new Option(data));
-    }); */
-});
+};
+deviceSelector.addEventListener("change", deviceSelectorChanged);
