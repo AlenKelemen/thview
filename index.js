43aa4e93f258720057e6286656a0248b6b06180a
Alen Kelemen <alen@edc.hr>
2021-03-30 09:37:24 +0200

    ...

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 import { elt } from "./util";
 import moment from "moment";
 
-const header = elt("h1", {style:'margin-top:0;margin-left:1rem'}, "Mjerenja na vodoopskrbnoj mreži");
+const header = elt(
+  "h1",
+  { style: "margin-top:0;margin-left:1rem" },
+  "Mjerenja na vodoopskrbnoj mreži"
+);
 document.body.appendChild(header);
 const startDate = elt("input", { type: "datetime-local" });
 const deviceSelector = elt("select", {});
 startDate.value = moment().subtract(1, "days").format("YYYY-MM-DDTHH:mm");
 const endDate = elt("input", { type: "datetime-local" });
 endDate.value = moment().format("YYYY-MM-DDTHH:mm");
-const pressure = elt("select", {size:'10',style:'width:300px'});
-const flow = elt("select", {size:'10',style:'width:300px'});
+const pressure = elt("select", { size: "10", style: "width:300px" });
+const flow = elt("select", { size: "10", style: "width:300px" });
+
+const tblPressure = elt(
+  "table",
+  {},
+  elt(
+    "thead",
+    {},
+    elt("tr", {}, elt("td", {}, "Vrijeme"), elt("td", {}, "Tlak bar"))
+  )
+);
 const fielset = elt(
   "fieldset",
-  {style:'margin-left:1em'},
+  { style: "margin-left:1em" },
   elt("label", {}, "Odaberi uređaj"),
   deviceSelector,
   elt("label", {}, "Početni datum"),
@@ -24,10 +38,11 @@ const fielset = elt(
   //elt('button',{type:'submit',onclick=}, 'Preuzmi'),
   elt("label", {}, "Protok l/s"),
   flow,
+
 );
 const form = elt("form", {}, fielset);
 document.body.appendChild(form);
-
+document.body.appendChild(  tblPressure)
 fetch("https://gis.edc.hr/imagisth/threport/device?info_id=eq.2")
   .then((response) => response.json())
   .then((data) => {
@@ -36,7 +51,7 @@ fetch("https://gis.edc.hr/imagisth/threport/device?info_id=eq.2")
       deviceSelector.options.add(o);
       if (i.device_id === 161) o.selected = true;
     }
-    deviceSelectorChanged()
+    deviceSelectorChanged();
   });
 
 const deviceSelectorChanged = () => {
@@ -47,38 +62,37 @@ const deviceSelectorChanged = () => {
   )
     .then((response) => response.json())
     .then((data) => {
-      let report = 'Tlak bar\n';
+      let report = "Tlak bar\n";
       const startUnixDate = moment(startDate.value).unix();
       const endUnixDate = moment(endDate.value).unix();
       for (const i of data) {
         if (i.date_part >= startUnixDate && i.date_part <= endUnixDate) {
-          const timeString = moment.unix(i.date_part).format('L LT')
+          const timeString = moment.unix(i.date_part).format("L LT");
           const o = new Option(timeString + " => " + i.pressure);
           pressure.options.add(o);
-          report = report + timeString + ";" + i.pressure + '\n'
+          report = report + timeString + ";" + i.pressure + "\n";
+          tblPressure.appendChild(elt('tr',{}, elt('td',{},timeString),elt('td',{},i.pressure + '')))
         }
       }
-      console.log(report)
+      console.log(report);
     });
-    flow.length = 0;
-    fetch(
-      "https://gis.edc.hr/imagisth/threport/flow_th?device_id=eq." + deviceId
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        let report = 'Protok l/s\n';
-        const startUnixDate = moment(startDate.value).unix();
-        const endUnixDate = moment(endDate.value).unix();
-        for (const i of data) {
-          if (i.date_part >= startUnixDate && i.date_part <= endUnixDate) {
-            const timeString = moment.unix(i.date_part).format('L LT')
-            const o = new Option(timeString + " => " + i.flow);
-            flow.options.add(o);
-          }
+  flow.length = 0;
+  fetch("https://gis.edc.hr/imagisth/threport/flow_th?device_id=eq." + deviceId)
+    .then((response) => response.json())
+    .then((data) => {
+      let report = "Protok l/s\n";
+      const startUnixDate = moment(startDate.value).unix();
+      const endUnixDate = moment(endDate.value).unix();
+      for (const i of data) {
+        if (i.date_part >= startUnixDate && i.date_part <= endUnixDate) {
+          const timeString = moment.unix(i.date_part).format("L LT");
+          const o = new Option(timeString + " => " + i.flow);
+          flow.options.add(o);
         }
-      });
+      }
+    });
 };
 
 deviceSelector.addEventListener("change", deviceSelectorChanged);
-startDate.addEventListener('change', deviceSelectorChanged);
-endDate.addEventListener('change', deviceSelectorChanged);
+startDate.addEventListener("change", deviceSelectorChanged);
+endDate.addEventListener("change", deviceSelectorChanged);
